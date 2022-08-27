69cae9d777fa66d52d7b49cee7f11dedc68d1b85
XinnChan <98129742+XinnChan@users.noreply.github.com>
2022-08-27 12:22:18 +0700

    Update XinnChan.js

--- XinnChan.js
+++ XinnChan.js
@@ -657,7 +657,7 @@ teks += `\n⭔ @${mem.id.split('@')[0]}\n`
 xinn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, { quoted: m })
 break
 
-case 'hacked':
+case 'hacked': 
 const mentionByReply = type == "extendedTextMessage" && m.message.extendedTextMessage.contextInfo != null ? m.message.extendedTextMessage.contextInfo.participant || "" : ""
 if (!mentionByReply) return setReply("Reply pesannya")
 if (mentionByReply == botNumber) {
@@ -665,6 +665,7 @@ xinn.sendMessage(from, { delete: { remoteJid: from, fromMe: true, id: m, partici
 } else if(mentionByReply !== botNumber && isBotGroupAdmins){
 if (!isGroupAdmins && !isOwner) return onlyAdmin()
 xinn.sendMessage(from, { delete: { remoteJid: from, fromMe: false, id: m, participant: mentionByReply } })
+}
 break
 
 case 'hack1':
